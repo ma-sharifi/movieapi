de2433b519d3d64777e9a447337e1cca80899633
Mahdi Sharifi <mahdi.elu@gmail.com>
2022-10-04 20:02:30 +0200

    first commit

---
+++ src/main/java/com/example/movieapi/MovieapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieapiApplication.class, args);
+	}
+
+}
